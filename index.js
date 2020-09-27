55b00a4d730604cc985f9267ae6ce258c6e66ed5
David Gasperoni <mcdado@gmail.com>
2020-09-28 00:53:46 +0200

    Correct breaking errors

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
-const path = require('path')
-const env = path.resolve(__dirname, '../.env')
-require('dotenv').config({ path: env })
+require('dotenv').config()
 
 const createChallenge = require('./lib/create-challenge')
 const deleteChallenge = require('./lib/delete-challenge')
@@ -10,14 +8,12 @@ const deleteChallenge = require('./lib/delete-challenge')
 const action = process.argv[2]
 const identifier = process.argv[3]
 const recordName = process.argv[4]
-// const zoneName = process.argv[5]
-// const nodeName = process.argv[6]
-const token = process.argv[7]
+const token = process.argv[5]
 
 if (action === 'create') {
   console.log(`Authenticating ${identifier} via DNS-01 challenge...`)
-  createChallenge(recordName, token)
+  createChallenge(identifier, recordName, token)
 } else if (action === 'delete') {
   console.log(`Deleting challenge for ${identifier}...`)
-  deleteChallenge(recordName)
+  deleteChallenge(identifier, recordName)
 }
