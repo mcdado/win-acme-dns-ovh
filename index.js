15136d6c253b3119d5fd60858cceea0688c34caa
David Gasperoni <web@3dprint.it>
2024-09-23 17:13:48 +0200

    Add debug package for silent logging

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const debug = require('debug')('win-acme-dns-ovh:index')
 
 // When the node process is executed in the context of wacs.exe, process.cwd() returns the directory where wacs.exe is
 // located and dotenv's default path is `path.resolve(process.cwd(), '.env')`. We force it here.
@@ -13,9 +14,9 @@ const recordName = process.argv[4]
 const token = process.argv[5]
 
 if (action === 'create') {
-  console.log(`Authenticating ${identifier} via DNS-01 challenge...`)
+  debug(`Authenticating ${identifier} via DNS-01 challenge...`)
   createChallenge(identifier, recordName, token)
 } else if (action === 'delete') {
-  console.log(`Deleting challenge for ${identifier}...`)
+  debug(`Deleting challenge for ${identifier}...`)
   deleteChallenge(identifier, recordName)
 }
